0eb816cae644a62effd81831126d3c9d37bd2c14
Paul Bailey <paul@neutrondrive.com>
2017-06-07 09:43:11 -0500

    reviewer app step 1

--- reviewer-app/app.js
+++ reviewer-app/app.js
@@ -4,8 +4,14 @@ var promise = require('bluebird');
 
 var app = express();
 
+app.set('view engine', 'hbs');
+
 app.use(body_parser.urlencoded({extended: false}));
 app.use('/static', express.static('public'));
+
+app.get('/', function (request, response) {
+  response.render('homepage.hbs', {});
+});
 
 app.listen(8000, function () {
   console.log('Listening on Port 8000');
