4d00cb66f347e5850cfff773657480a30ea31b4f
Paul Bailey <paul@neutrondrive.com>
2017-06-28 11:27:13 -0500

    react material ui

--- form-app/src/App.js
+++ form-app/src/App.js
@@ -2,18 +2,21 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import MyForm from './myform';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import {red700} from 'material-ui/styles/colors';
+
+const theme = getMuiTheme({
+  palette: {primary1Color: red700}
+});
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
+      <MuiThemeProvider muiTheme={theme}>
         <MyForm/>
-      </div>
+      </MuiThemeProvider>
     );
   }
 }
--- form-app/src/myform.jsx
+++ form-app/src/myform.jsx
@@ -1,5 +1,14 @@
 import React, { Component } from 'react';
 
+import AppBar from 'material-ui/AppBar';
+import {Card, CardActions, CardTitle, CardText} from 'material-ui/Card';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
+import SelectField from 'material-ui/SelectField';
+import MenuItem from 'material-ui/MenuItem';
+
+import './myform.css';
+
 class MyForm extends Component {
   constructor (props) {
     super(props);
@@ -18,6 +27,10 @@ class MyForm extends Component {
     this.setState(new_state);
   }
   
+  update_select = (event, index, value) => {
+    this.setState({color: value});
+  }
+  
   handleSubmit (event) {
     console.log('submitted:', this.state);
     event.preventDefault();
@@ -25,17 +38,30 @@ class MyForm extends Component {
   
   render () {
     return (
-      <form onSubmit={event => this.handleSubmit(event)}>
-        <label>Your Name</label>
-        <input type="text" value={this.state.name} onChange={event => this.update_state(event, 'name')}/>
-        <br/><br/>
-        <select value={this.state.color} onChange={event => this.update_state(event, 'color')}>
-          <option value="red">Red</option>
-          <option value="blue">Blue</option>
-        </select>
-        <br/><br/>
-        <button type="submit">Submit</button>
-      </form>
+      <div>
+        <AppBar title="My Awesome Form"/>
+        <form onSubmit={event => this.handleSubmit(event)}>
+          <Card className="md-card">
+            <CardTitle title="My Form"/>
+            <CardText>
+              <TextField floatingLabelText="Your Name"
+              defaultValue={this.state.name}
+              onChange={event => this.update_state(event, 'name')}/>
+              <br/><br/>
+              <SelectField
+                floatingLabelText="Color"
+                value={this.state.color}
+                onChange={this.update_select}>
+                <MenuItem value="red" primaryText="Red"/>
+                <MenuItem value="blue" primaryText="Blue"/>
+              </SelectField>
+            </CardText>
+            <CardActions>
+              <RaisedButton type="submit" label="Submit" primary={true}/>
+            </CardActions>
+          </Card>
+        </form>
+      </div>
     );
   }
 }
