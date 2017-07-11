1c460f77feec227e372dd156a993e59bde657a93
Paul Bailey <paul@neutrondrive.com>
2017-07-11 09:46:38 -0500

    jwt start

--- chat/app.js
+++ chat/app.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var app = express();
+var jwt = require('jsonwebtoken');
+
+var SECRET = 'SUPER-SECRET';
 
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
@@ -7,7 +10,8 @@ var io = require('socket.io')(http);
 app.set('view engine', 'hbs');
 app.use('/static', express.static('static'));
 app.get('/', function (request, response) {
-  response.render('chat.hbs');
+  var token = jwt.sign({user: 'pizzapanther'}, SECRET, {expiresIn: 5});
+  response.render('chat.hbs', {token: token});
 });
 
 io.on('connection', function (client) {
@@ -28,6 +32,14 @@ io.on('connection', function (client) {
     });
     
     client.on('incoming', function (msg) {
+      try {
+        var decoded = jwt.verify(msg.token, SECRET);
+      } catch (e) {
+        io.to(msg.room).emit('chat-msg', 'Sorry Loser');
+        return;
+      }
+      
+      console.log(decoded);
       io.to(msg.room).emit('chat-msg', msg.msg);
     });
   });
